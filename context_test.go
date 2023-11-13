6475f9b9383afed86bf2d5bfcd5aa9b0b0b5b7de
Ernest Micklei <ernest@ag5.com>
2023-11-13 16:42:14 +0100

    add WithLoggerContext

--- context_test.go
+++ context_test.go
@@ -2,6 +2,7 @@ package cslog
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"log/slog"
@@ -12,4 +13,6 @@ func TestWithLogger(t *testing.T) {
 	ctx := WithLogger(context.Background(), mylog)
 	hislog := Ctx(ctx)
 	hislog.Info("Hello, world!")
+	err := errors.New("bummer")
+	hislog.Error("Panic", "err", err, slog.Any("err", err))
 }
